014083cfddb169d433e70910d1c1703ff5532be6
Gadom654 <dominikgadomski654@gmail.com>
2025-10-17 09:30:12 +0200

    INDIB2

--- Game/Cloud_Hero_for_Data_Analysts/Ingesting_New_Datasets_into_BigQuery/README.md
+++ Game/Cloud_Hero_for_Data_Analysts/Ingesting_New_Datasets_into_BigQuery/README.md
@@ -1,5 +1,5 @@
 How to complete this lab:
 1. Run in google cloud shell:
-curl -L -O "https://raw.githubusercontent.com/Gadom654/Google_qwik_labs/main/Game/Cloud_Hero_for_Data_Analysts/Ingesting_New_Datasets_into_BigQuery"
-cd Ingesting_New_Datasets_into_BigQuery
+curl -L -O "https://raw.githubusercontent.com/Gadom654/Google_qwik_labs/main/Game/Cloud_Hero_for_Data_Analysts/Ingesting_New_Datasets_into_BigQuery/script.sh"
+curl -L -O "https://raw.githubusercontent.com/Gadom654/Google_qwik_labs/main/Game/Cloud_Hero_for_Data_Analysts/Ingesting_New_Datasets_into_BigQuery/products.csv"
 bash script.sh
--- Game/Cloud_Hero_for_Data_Analysts/Ingesting_New_Datasets_into_BigQuery/script.sh
+++ Game/Cloud_Hero_for_Data_Analysts/Ingesting_New_Datasets_into_BigQuery/script.sh
@@ -2,33 +2,9 @@
 MY_PROJECT=$(gcloud config get-value project)
 bq mk ecommerce
 bq load --autodetect --source_format=CSV ecommerce.products ./products.csv
-bq query --use_legacy_sql=false <<EOF
-#standardSQL
-SELECT
-  *
-FROM
-  ecommerce.products
-ORDER BY
-  stockLevel DESC
-LIMIT  5
-EOF
-bq load --autodetect -write_disposition=WRITE_TRUNCATE --source_format=CSV ecommerce.products gs://spls/gsp411/exports/products.csv
-bq query --use_legacy_sql=false <<EOF
-#standardSQL
-SELECT
-  *,
-  SAFE_DIVIDE(orderedQuantity,stockLevel) AS ratio
-FROM
-  ecommerce.products
-WHERE
-# include products that have been ordered and
-# are 80% through their inventory
-orderedQuantity > 0
-AND SAFE_DIVIDE(orderedQuantity,stockLevel) >= .8
-ORDER BY
-  restockingLeadTime DESC
-EOF
-echo "What is the link"
+bq load --autodetect --replace --source_format=CSV ecommerce.products gs://spls/gsp411/exports/products.csv
+echo "Run query in 4th task manualy and follow 5th task untli link is generated"
+echo "What is the link?"
 read link
 bq mk --external_table_definition='ecommerce.products_comments@Drive=[{"sourceFormat":"GOOGLE_SHEETS","sourceUris":["$link"],"headers":1}]'
 bq query --use_legacy_sql=false <<EOF
